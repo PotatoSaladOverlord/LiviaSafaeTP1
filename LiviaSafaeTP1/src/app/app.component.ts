a659969977441c2efaa81f5c74fc4ab12730bb1e
Livia Leroy-Stone <livia@Ordinateur-portable-de-Livia.local>
2024-10-25 15:27:09 +0200

    fin_header_footer

--- LiviaSafaeTP1/src/app/app.component.ts
+++ LiviaSafaeTP1/src/app/app.component.ts
@@ -2,11 +2,13 @@ import {Component, Input} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {CarreContenantComponent} from "./carre-contenant/carre-contenant.component";
 import {NgStyle} from "@angular/common";
+import {HeaderComponent} from "./header/header.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CarreContenantComponent, NgStyle],
+  imports: [RouterOutlet, CarreContenantComponent, NgStyle, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
